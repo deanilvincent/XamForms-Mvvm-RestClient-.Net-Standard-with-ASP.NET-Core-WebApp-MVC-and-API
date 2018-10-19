40bb8e8db58708781fa9fdec248abc888a30cfd6
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-10-19 14:31:06 +0800

    initial commit

---
+++ MyAppSolution/WebApp/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext (DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:sea0001.database.windows.net,1433;Initial Catalog=mydb01;Persist Security Info=False;User ID=saa;Password=Pr0xxxy321;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+        public DbSet<WebApp.Models.Product> Product { get; set; }
+    }
+}
---
+++ MyAppSolution/WebApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
